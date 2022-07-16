ce2a38e5dcd0277abb84da0b2a5a53344dbdb27a
Adeleke Oluwatobi <57203812+tobiadiks@users.noreply.github.com>
2022-07-16 19:21:32 +0100

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,36 +1,22 @@
 function findSum(num,target){
-
+	let ans;
   //returns an empty array for an empty input
-
   if (num==undefined ||num.length==0) {
-
     console.log([])
-
   }
-
   else{
-
     for(let i=0;i<num.length;i++){
-
       num.map((value)=>{
-
         if(num[i]+value==target){
-
-          console.log([num[i],value])
-
+          ans=[num[i],value]
+          console.log(ans)
+          
         }
-
       })
-
       //breaks for loop immediately after getting first single solution instead of running through all the array, i did this to manage system resource and memory.
-
-      break;
-
+     if(ans)break;
     }
-
   }
-
 }
 
-findSum([2,11,7,15],9)
-
+findSum([2,11,7,15,3,6],17)
